3e13e3c3afcc45ba925e778d1dd0d8f610245165
HectorRivera <redmagix2000@gmail.com>
2023-01-29 15:35:58 -0600

    comments

--- C++/CSCI_3333/HW1/trendtracker.cpp
+++ C++/CSCI_3333/HW1/trendtracker.cpp
@@ -56,26 +56,36 @@ int Trendtracker::popularity(string name){
 }
 //////////////////////////////////////////
 string Trendtracker::top_trend(){
+    // size zero do nothing
     if(E.size()==0){
         return "";
     }
-    int cookie = -1;
+    //placeholder for current highest popularity
+    int tempPR = -1;
+    //empty string for current trending hashtag
     string Top_Trending;
-    // deadlift ? bench ? squat ?
+    //iterate through E
     for(auto i : E){
-        if(i.pop > cookie){
-            cookie = i.pop;
+        // if i elements pop is greater than tempPR
+        // set tempPR to i.pop and top_tending to i.hashtag 
+        if(i.pop > tempPR){
+            tempPR = i.pop;
             Top_Trending = i.hashtag;
         }
     }
+    //return the top trending hashtag
     return Top_Trending;
 }
 void Trendtracker::top_three_trends(vector<string> &T){
     T.clear();
+    // keep track of popularity and hashtags
     int max1=-1, max2=-1, max3=-1;
     string trend1, trend2, trend3;
+    // iterate through E
     for(auto &i : E)
     {
+        // checking the top 3 trends 
+        // then updating top 3 accordingly 
         if(i.pop > max1)
         {
             max3 = max2;
@@ -110,22 +120,32 @@ void Trendtracker::top_three_trends(vector<string> &T){
 
 }
 void Trendtracker::remove(string ht){
+    //iterate through vector 
     for(int i = 0; i < E.size(); i++)
+        // check for ht in the vector
         if (E[i].hashtag == ht)
+            //remove the element at the specified index
             E.erase(E.begin() + i); 
 }
 void Trendtracker::top_k_trends(vector<string> &T, int k){
+    //makesure vector has nothing in it
     T.clear();
+    //iterate through k times
     for(int i = 0; i < k ;i++){
+        //placeholders for top trending hashtags and idex of hashtag
         int bigPOP = 0, idx_max = -1;
+        //iterate through E
         for(int j = 0; j < E.size();j++){
+            // check if the popularity has a higher pop than current
             if(E[j].pop > bigPOP){
+                //update bigPOP and idx_max
                 bigPOP = E[j].pop;
                 idx_max = j;
             }
         }
+        //add the current top trend to vector T
         T.push_back(E[idx_max].hashtag);
+        //set pop to zero to avoid duplicate top hashtags
         E[idx_max].pop = 0;
     }
-    
 }
