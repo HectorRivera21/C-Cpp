7993ed831667a54062a0a4607a90b255a55b289b
HectorRivera <redmagix2000@gmail.com>
2023-03-20 23:43:12 -0500

    NA

---
+++ C++/CSCI_3333/HW6/minpriorityqueue.h
@@ -0,0 +1,81 @@
+#ifndef MINPRIORITYQUEUE_H
+#define MINPRIORITYQUEUE_H
+
+// NOTE: You may not include any other libraries!
+#include <unordered_map>
+#include <vector>
+#include <utility> // Has pair and swap
+
+using namespace std;
+
+template <typename T>
+class MinPriorityQueue
+{
+	// For the mandatory running times below:
+	//
+	// n is the number of elements in the MinPriorityQueue.
+	//
+	// Assume that the operations of unordered_map are O(1) time 
+	// (they are average case, but not worst-case).
+
+	public:
+		// Creates an empty MinPriorityQueue
+		MinPriorityQueue()
+		{
+			// TODO
+		}
+
+		// Returns the number of elements in the MinPriorityQueue.
+		//
+		// Must run in O(1) time.
+		int size()
+		{
+			// TODO	
+		}	
+
+		// Pushes a new value x with priority p
+		// into the MinPriorityQueue.
+		//
+		// Must run in O(log(n)) time.		 
+		void push(T x, int p)
+		{
+			// TODO
+		}
+
+		// Returns the value at the front of the MinPriorityQueue.
+		// Undefined behavior if the MinPriorityQueue is empty.
+		// 
+		// Must run in O(1) time.
+		T front()
+		{
+			// TODO	
+		}
+
+		// Removes the value at the front of the MinPriorityQueue.
+		// Undefined behavior if the MinPriorityQueue is empty.
+		//
+		// Must run in O(log(n)) time. 
+		void pop()
+		{
+			// TODO	
+		}
+
+		// If x is in the MinPriorityQueue 
+		// with current priority at least new_p,  
+		// then changes the priority of x to new_p.
+		// Undefined behavior otherwise.
+		//
+		// Must run in O(log(n)) time. 
+		void decrease_key(T x, int new_p)
+		{
+			// TODO
+		}
+
+	private:
+		// You don't need any other instance variables,
+		// but you can add some if you want to.
+		vector< pair<T, int> > H; // The heap.
+		unordered_map<T, int> I;  // Maps values to their indices in H.
+};
+
+#endif 
