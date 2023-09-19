9325469d57da07550ba17339750ce3fc98c91770
Hector Rivera <redmagix2000@gmail.com>
2023-09-18 23:30:33 -0700

    ALMOST

--- python/DataMining/main.cpp
+++ python/DataMining/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define ARRAY_LEN(xs) sizeof((xs))/sizeof((xs)[0])
+using namespace std;
 
 typedef struct{
     std::vector<int> w1;
@@ -8,18 +8,26 @@ typedef struct{
     //Class A = 1, Class B = 0;
     std::vector<bool> CLASS;
 }EQ;
-///adasdasd
-std::vector<float> cost(EQ a,int* x, int* y)
+
+vector<float> cost(EQ a,vector<int> x, vector<int>y)
 {   
     std::vector<float>sum;
-    auto n = ARRAY_LEN(x)+1;
+    auto n = x.size();
     for(int i = 0; i<3;++i){
         float add = 0.0f;
-        for(int j = 0; j<=n;++j){
-            float top = (x[j]+y[j]+a.w0[i]);
-            float bot = sqrtf(pow(a.w1[i],2)+pow(a.w2[i],2));
+        for(int j = 0; j<n;++j){
+            float top;
+            if(i == 1)
+                top = (x[j]-y[j]+a.w0[i]);
+            else
+                top = (x[j]+y[j]+a.w0[i]);
+            float bot = sqrtf((float)(pow(a.w1[i],2)+pow(a.w2[i],2)));
             float loss = top/bot;
-            if(a.CLASS[j] != false || loss > 0 )
+            if(a.CLASS[j] != false && loss < 0 )
+            {
+                add += abs(loss);
+            }
+            else if (a.CLASS[j] != true && loss > 0 )
             {
                 add += abs(loss);
             }
@@ -30,17 +38,17 @@ std::vector<float> cost(EQ a,int* x, int* y)
 }
 
 int main(){
+
     EQ a;
-    int x[] = {0,3,4,8};
-    int y[] = {0,3,5,0};
+    vector<int> x = {0,3,4,8};
+    vector<int> y = {0,3,5,0};
     a.w1 = {1,1,1}; 
     a.w2 = {1,-1,2};
     a.w0 = {-12,5,3};
-    a.CLASS ={0,0,1,1};
-    std::vector<float>print =  cost(a,x,y);
+    a.CLASS ={false,false,true,true};
+    vector<float>print =  cost(a,x,y);
     for(int i = 0; i<print.size(); ++i){
         printf("%d. %f\n",i,print[i]);
     }
-    
     return 0;
 }
