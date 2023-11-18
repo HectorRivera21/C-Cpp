f6b6bd12e24259f0421f854c7e272c0c11b28c81
Hector <redmagix2000@gmail.com>
2023-11-18 00:37:39 -0600

    new data

---
+++ python/Databases/Summary.py
@@ -0,0 +1,8 @@
+import pandas as PD
+Musicians = PD.read_csv('Data/musician.csv')
+MusicianAlbum = PD.read_csv('Data/musician-album.csv')
+Instruments = PD.read_csv('Data/instruments.csv')
+Albums = PD.read_csv('Data/album.csv')
+AlbumInstrumnet = PD.read_csv('Data/album-instruments.csv')
+
+print(Musicians)
