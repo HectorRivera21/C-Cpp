4f099f133b0f3c9fc97575751aae3dddeff70a31
Hector <redmagix2000@gmail.com>
2023-11-21 00:06:05 -0600

    nah

--- Racket/WindowRaylib.rkt
+++ Racket/WindowRaylib.rkt
@@ -12,60 +12,53 @@
   (InitWindow screen-width screen-height "Moonshot")
   (SetTargetFPS 60))
 
-(define (clamp value min-value max-value)
-  (min max-value (max min-value value)))
-
 (define (cleanup-game)
   ;; Unload game resources and close the window
   (CloseWindow))
 
 (define (game-loop)
+  (define square-pos (make-Vector2 100.0 100.0))  ; Initialize the square position
   (let loop ()
     (when (not (WindowShouldClose))
-      (update-game)  ; Update game state
-      (draw-game)    ; Draw game
+      (update-game square-pos)  ; Update game state
+      (draw-game square-pos)    ; Draw game
       (sleep (/ 1 60.0))
       (loop))))
 
-(define (update-square square-pos dt)
+(define (clamp value min-value max-value)
+  (min max-value (max min-value value)))
+
+(define (update-square pos dt)
   ;; Update square position based on user input
-  (let* ((speed 5))
-    (cond [(IsKeyDown KEY_A)
-      (set-Vector2-x! square-pos (- (Vector2-x square-pos) (* speed dt)))])
-    ([(IsKeyDown KEY_D)
-      (set-Vector2-x! square-pos (+ (Vector2-x square-pos) (* speed dt)))])
-    ([(IsKeyDown KEY_W)
-      (set-Vector2-y! square-pos (- (Vector2-y square-pos) (* speed dt)))])
-    ([(IsKeyDown KEY_S)
-      (set-Vector2-y! square-pos (+ (Vector2-y square-pos) (* speed dt)))])
+  (let* ((speed 250))
+    (when (IsKeyDown KEY_A)
+      (set-Vector2-x! pos (- (Vector2-x pos) (* speed dt))))
+    (when (IsKeyDown KEY_D)
+      (set-Vector2-x! pos (+ (Vector2-x pos) (* speed dt))))
+    (when (IsKeyDown KEY_W)
+      (set-Vector2-y! pos (- (Vector2-y pos) (* speed dt))))
+    (when (IsKeyDown KEY_S)
+      (set-Vector2-y! pos (+ (Vector2-y pos) (* speed dt))))
 
     ;; Ensure the square stays within the screen bounds
-    (set-Vector2-x! square-pos (clamp (Vector2-x square-pos) 0.0 (- screen-width 50.0)))
-    (set-Vector2-y! square-pos (clamp (Vector2-y square-pos) 0.0 (- screen-height 50.0)))))
+    (set-Vector2-x! pos (clamp (Vector2-x pos) 0.0 (- screen-width 50.0)))
+    (set-Vector2-y! pos (clamp (Vector2-y pos) 0.0 (- screen-height 50.0)))))
 
-
-(define (update-game)
+(define (update-game square-pos)
   ;; Update game state
   (set! dt (min FRAME (GetFrameTime)))
-  (define x 1)
-  (when (<= x 0)
-    (update-square (make-Vector2 100.0 100.0) dt))  ; Update square position
-  (set! x 15)
+  (update-square square-pos dt)
+
   ; Other game logic...
   )
 
-(define (draw-game)
+(define (draw-game square-pos)
   ;; Draw game
   (BeginDrawing)
   (ClearBackground BLACK)
 
   ;; Draw the square
-  (let ((square-pos (make-Vector2 100.0 100.0))
-        (square-size (make-Vector2 50.0 50.0))
-        (square-color WHITE))
-    (DrawRectangleV (make-Vector2 (Vector2-x square-pos) (Vector2-y square-pos))
-                    (make-Vector2 (Vector2-x square-size) (Vector2-y square-size))
-                    square-color))
+  (DrawRectangleV square-pos (make-Vector2 52.0 52.0) WHITE)
 
   (EndDrawing))
 
