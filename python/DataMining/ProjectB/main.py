59d7a4e212abe4b6be6568f6383f30e3bc998c01
Hector <redmagix2000@gmail.com>
2023-11-27 05:00:21 -0600

    NA

--- python/DataMining/ProjectB/main.py
+++ python/DataMining/ProjectB/main.py
@@ -11,12 +11,15 @@ df['brew_data'] = df['brew_data'].apply(lambda x: x.split(';'))
 brew_data =  0.0
 for index, row in df.iterrows():
     tasty = row['tasty']
-    brew_data = [float(value) if value != '' else 0.0 for value in row['brew_data']]  # Convert values to float
-    plt.plot(brew_data, label=tasty) #if you want multilines
+    brew_data = [float(value) if value != '' else 0.0 for value in row['brew_data']]
+    time_points = [i/10 for i in range(len(brew_data))]
+    print(time_points)
+
+    plt.plot(time_points,brew_data, label=tasty) #if you want multilines
     plt.xlabel('Index')
     plt.ylabel('Brew Data Value')
     plt.title('Brew Data for Different Tasty')
     plt.legend()
     plt.show()
-    plt.savefig('${}.png', bbox_inches='tight')
+    # plt.savefig('${}.png', bbox_inches='tight')
 #plt.plot(brew_data, label=tasty) #single line
--- python/Databases4343/Summary.py
+++ python/Databases4343/Summary.py
@@ -1,37 +1,49 @@
 import pandas as pd
 import sqlite3 as sql
+from concurrent.futures import ThreadPoolExecutor
+from itertools import zip_longest
 
-# data extraction
+#taDataClean extraction
 Musicians = pd.read_csv('Data/musician.csv')
 MusicianAlbum = pd.read_csv('Data/musician-album.csv')
 Instruments = pd.read_csv('Data/instrument.csv')
 Albums = pd.read_csv('Data/album.csv')
 AlbumInstrumnet = pd.read_csv('Data/album-instrument.csv')
-# print(Music
-Dlist = [Musicians, Instruments, Albums]
-Tname = ['Musicians', 'Intruments', 'Albums']
-# cleaing up data?
-Musicians.columns = Musicians.columns.str.strip()
 
-#connection
+#list for simplitcity sake
+Dlist = [Musicians, Instruments, Albums, MusicianAlbum, AlbumInstrumnet]
+Tname = ['Musicians', 'Intruments', 'Albums','MusicianAlbum','AlbumInstrumnet']
+#open DB connection
 connection = sql.connect('DB/Music.db')
-
 # data to SQLite
 for i in range(len(Dlist)):
+    #supposed cleaning up data ?????
+    Dlist[i].columns = Dlist[i].columns.str.strip()
+    #inject to the SQL DB
     Dlist[i].to_sql(f'{Tname[i]}', connection, if_exists='replace',index=False)
-
-cur = connection.cursor()
-
-cur.execute('SELECT name FROM Musicians')
-
-rows = cur.fetchall()
-i = 1
-for row in rows:
-    for str in row:
-        print(f'{i}. {str}')
-        if i == len(rows):
-            print(f'Total Musicians: {i}')
-        i+=1
-
-#close connection
+#close the connection
 connection.close()
+
+
+#func to retrieve data from DB using queries supplied
+def fetch_data(query):
+    connection = sql.connect("DB/Music.db")
+    cursor = connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    connection.close()
+    return data
+
+#list of queries i want to use
+queries = ["SELECT name FROM Musicians", "SELECT name FROM Albums", "SELECT type FROM Instruments"]
+
+#extract all data through parallelism 
+with ThreadPoolExecutor() as executor:
+    Data = list(executor.map(fetch_data, queries))
+
+
+totals = [sum(1 for row in dataset) for dataset in Data]
+print("Musicans\tAlbums\tInstruments")
+for row in zip_longest(*Data, fillvalue=""):
+    print("\t ".join(map(str,row)))
+print("\t".join(f"Total: {total}"for total in totals))
