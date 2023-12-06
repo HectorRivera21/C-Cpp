02d951ac8623f672961d4f757e986778febc85bb
Hector <redmagix2000@gmail.com>
2023-12-06 02:22:35 -0600

    NAH

--- python/Databases4343/Update.py
+++ python/Databases4343/Update.py
@@ -1,2 +1,24 @@
+import argparse as AP
 import sqlite3 as SQL
-print("h")
+import sys
+
+parser = AP.ArgumentParser()
+parser.add_help
+parser.add_argument('--add', action="store_true", default=False, help="add to table")
+parser.add_argument('--delete', action="store_true", default=False, help="delete from table")
+    
+parser.add_argument('--table', type=str, default='', help="target table")
+parser.add_argument('--record', type=str, default='', help="Info to be added/deleted \n 'data,adat,atad,dadw' ")
+
+args = parser.parse_args()
+
+if args.add == True and args.delete == True:
+    sys.exit('to many args')
+
+if args.add == True:
+    
+    sys.exit(f"added: {args.record} to Table: {args.table}")
+
+if args.delete == True:
+
+    sys.exit(f"deleted: {args.record} from Table: {args.table}")
