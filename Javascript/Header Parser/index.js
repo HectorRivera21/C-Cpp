c7fbb4f3c711283aee12f43157479f58d428ba84
Hector Rivera <redmagix2000@gmail.com>
2024-01-03 01:06:22 -0800

    finished

--- Javascript/Header Parser/index.js
+++ Javascript/Header Parser/index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ipaddress:ip,language:language,software:software });
 });
 
 // listen for requests :)
