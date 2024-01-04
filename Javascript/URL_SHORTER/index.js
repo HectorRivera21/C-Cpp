d3c90ed9cf0cc02a2142fd1cd67fae17db8f1934
Hector <redmagix2000@gmail.com>
2024-01-03 19:34:53 -0600

    url hasher added problem with non http url getting though

--- Javascript/URL_SHORTER/index.js
+++ Javascript/URL_SHORTER/index.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const { urlencoded } = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
+let db = {};
+String.prototype.hashCode = function() {
+  var hash = 0,
+    i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,11 +29,34 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+//parse json application 
+app.use(bodyParser.json());
+
+app.use(urlencoded({extended:true}));
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url;
+  console.log(db);
+  try {
+    new URL(url);
+    let short_url = url.hashCode();
+    db[short_url]=url;
+    console.log(db);
+    res.json({original_url: url, short_url:short_url});
+  }catch(e){
+    res.json({error:"Invalid URL"});
+  }
 });
 
+app.get('/api/shorturl/:url', (req, res)=>{
+  let url = req.params["url"];
+  if(url in db){
+    res.redirect(db[url]);
+  }
+  else{
+    res.json({error:"invalid url"});
+  }
+  
+});
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
