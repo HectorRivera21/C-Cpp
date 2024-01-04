15ef76858fc931e738377d2b6b17ccca8e92e6d5
Hector <redmagix2000@gmail.com>
2024-01-03 22:28:07 -0600

    Mongo DB added

--- Javascript/URL_SHORTER/index.js
+++ Javascript/URL_SHORTER/index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dns = require('dns');
 const { urlencoded } = require('body-parser');
 const bodyParser = require('body-parser');
 const app = express();
-let db = {};
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+const SchemaURL = new Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+const Url = Model("Url", SchemaURL);
 String.prototype.hashCode = function() {
   var hash = 0,
     i, chr;
@@ -15,8 +26,8 @@ String.prototype.hashCode = function() {
     hash = ((hash << 5) - hash) + chr;
     hash |= 0; // Convert to 32bit integer
   }
-  return hash/1000;
-}
+  return hash<0 ? Math.floor(hash/-1000) : Math.floor(hash/1000);
+};
 const isValidHttpUrl = (string) =>{
   try {
     const newUrl = new URL(string);
@@ -24,7 +35,18 @@ const isValidHttpUrl = (string) =>{
   } catch (err) {
     return false;
   }
-}
+};
+const createURL = (Original_Url, Short_Url, done)=>{
+
+  let url = new Url(
+  {
+    original_url: Original_Url,
+    short_url: Short_Url,
+  });
+  url.save((err,data)=>{
+    if(err) return console.error(err);
+  });
+};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -48,20 +70,21 @@ app.post('/api/shorturl', function(req, res) {
   }
   else{
     let short_url = url.hashCode();
-    db[short_url]=url;
+    createURL(url,short_url);
     res.json({original_url: url, short_url:short_url});
   }
 });
 
 app.get('/api/shorturl/:url', (req, res)=>{
   let url = req.params["url"];
-  if(url in db){
-    res.redirect(db[url]);
-  }
-  else{
-    res.json({error:"invalid url"});
-  }
-  
+  Url.findOne({short_url: url}, (err, data)=>{
+    if(err||!data) res.json({error:"invalid url"});
+    else 
+    {
+      res.redirect(data.original_url);
+     
+    }
+  })  
 });
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
