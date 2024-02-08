8de61e9be4d4a92a0a3c62834ccfd3a7bca46de1
Hector Rivera <redmagix2000@gmail.com>
2024-02-07 23:10:46 -0800

    NAH

--- Rust/hello_world/src/main.rs
+++ Rust/hello_world/src/main.rs
@@ -1,16 +1,7 @@
-fn foo(strs: Vec<String>) -> String {
-    strs.into_iter()
-        .reduce(|a, b| {
-            a.chars()
-                .zip(b.chars())
-                .take_while(|(c, d)| c == d)
-                .map(|(x, _)| x)
-                .collect()
-        })
-        .unwrap()
+
+fn remove_duplicates(_nums: &mut Vec<i32>) -> i32 {
+    0
 }
 
 fn main() {
-    let strs: Vec<String> = ["flow", "flower", "flew"].map(String::from).to_vec();
-    println!("{}",foo(strs));
 }
---
+++ Rust/hello_world/target/debug/deps/hello_world-acf5bc543a12b810.d
@@ -0,0 +1,5 @@
+C:\Users\hecto\Desktop\coding\C-Cpp\Rust\hello_world\target\debug\deps\libhello_world-acf5bc543a12b810.rmeta: src/main.rs
+
+C:\Users\hecto\Desktop\coding\C-Cpp\Rust\hello_world\target\debug\deps\hello_world-acf5bc543a12b810.d: src/main.rs
+
+src/main.rs:
